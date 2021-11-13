0f47041067683152c80abf5feae83a84e938ca29
combo819 <617512696@qq.com>
2021-11-12 20:11:21 -0500

    docs: ✏️ add get-start text in doc

--- packages/documentation/pages/_app.js
+++ packages/documentation/pages/_app.js
@@ -1,4 +1,6 @@
 import 'nextra-theme-docs/style.css';
+import Prism from 'prism-react-renderer/prism'
+(typeof global !== "undefined" ? global : window).Prism = Prism
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
